4856c246ec263032f90ce368b277f5d959170147
SHIVANIDUFARE124 <66694740+SHIVANIDUFARE124@users.noreply.github.com>
2020-08-28 19:14:10 +0530

    Add files via upload

---
+++ print sin cos tan value.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float n;
+	printf("enter the value of n:");
+	scanf("%d",&n);
+	printf("\n sin value:%f",sin(n));
+	printf("\n cos value:%f",cos(n));
+	printf("\n tan value:%f",tan(n));
+	return 0;
+}
